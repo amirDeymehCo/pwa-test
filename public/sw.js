fda60f08a87c11b9cbc429201533f0bee6f10563
amir <amirdeymeh8@gmail.com>
2022-08-18 13:37:35 +0330

    add all file page index

--- public/sw.js
+++ public/sw.js
@@ -4,6 +4,17 @@ self.addEventListener("install", (event) => {
       console.log("فایل ها در حافظه ی cachts ذخیره شدن:)");
       cache.add("/");
       cache.add("/index.html");
+      cache.add("/src/css/app.css");
+      cache.add("/src/css/feed.css");
+      cache.add("/src/js/app.js");
+      cache.add("/src/js/feed.js");
+      cache.add("/src/js/material.min.js");
+      cache.add("/src/images/main-image.jpg");
+      cache.add("https://fonts.googleapis.com/css?family=Roboto:400,700");
+      cache.add("https://fonts.googleapis.com/icon?family=Material+Icons");
+      cache.add(
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+      );
     })
   );
 });
